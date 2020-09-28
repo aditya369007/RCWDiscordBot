90eead5012bee299d1548c03255feff76455e503
Aditya Mulampally <aditya.mulampally@gmail.com>
2020-09-27 23:33:38 -0500

    Added a bot command to feed the table data to the database file

--- discordBot.py
+++ discordBot.py
@@ -1,5 +1,6 @@
 import os
 import random
+import sqlite3
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -29,6 +30,18 @@ async def randreport(ctx):
 
 @bot.command(name='report', help='Personally report someone', usage='!report @member')
 async def report(ctx, arg):
-    member = ctx.guild.get_member_named(arg)
     await ctx.send('{0} has been reported by {1.author.mention}'.format(arg, ctx))
+
+
+@bot.command(name='feedTableData')
+@commands.is_owner()
+async def feedTableData(ctx):
+    conn = sqlite3.connect("RCWdatabase.db")
+    cursor = conn.cursor()
+    memberList = []
+    for member in ctx.guild.members:
+        memberList.append((member.name, 0, 0, 10))
+    cursor.executemany('INSERT INTO RCWDB VALUES (?,?,?,?)', memberList)
+    conn.commit()
+    conn.close()
 bot.run(TOKEN)
