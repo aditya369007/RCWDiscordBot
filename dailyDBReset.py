38138be2d347e662d558e90ba5ef9d2ef414e53c
Aditya Mulampally <aditya.mulampally@gmail.com>
2020-09-29 23:16:03 -0500

    Resets the currency of each member to default 10 each day

---
+++ dailyDBReset.py
@@ -0,0 +1,17 @@
+import sqlite3
+import schedule
+import time
+
+def resetRCWDBCurrency():
+    conn = sqlite3.connect("RCWdatabase.db")
+    cursor = conn.cursor()
+    cursor.execute('UPDATE RCWDB SET Currency = 10')
+    conn.commit()
+    conn.close()
+    print("Database Reset!!")
+
+schedule.every().day.at("6:00").do(resetRCWDBCurrency)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
